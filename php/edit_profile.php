a5f7ea6420f205a07d65053d4df9f3f75a5a5ffd
aashishhhhhh <47654151+aashishhhhhh@users.noreply.github.com>
2024-03-29 18:45:41 -0400

    changes made including logout

--- php/edit_profile.php
+++ php/edit_profile.php
@@ -19,22 +19,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Get form data and sanitize it
     $firstname = mysqli_real_escape_string($conn, $_POST["firstname"]);
     $lastname = mysqli_real_escape_string($conn, $_POST["lastname"]);
-    $email = mysqli_real_escape_string($conn, $_POST["email"]);
     $phone = mysqli_real_escape_string($conn, $_POST["phone"]);
-    $password = mysqli_real_escape_string($conn, $_POST["password"]);
     $address = mysqli_real_escape_string($conn, $_POST["address"]);
-    $postal = mysqli_real_escape_string($conn, $_POST["postalcode"]);
-    $dob = mysqli_real_escape_string($conn, $_POST["dob"]);
-    $gender = mysqli_real_escape_string($conn, $_POST["mySelect"]);
-    $hashed_password = password_hash($password, PASSWORD_BCRYPT);
     $emailToUpdate = $_SESSION['email'];
 
     // Update statement
-    $updateQuery = "UPDATE users SET first_name = ?, last_name = ?, email = ?, phone = ?, password = ?, address = ?, postal_code = ?, dob = ?, gender = ?  WHERE email = ?";
+    $updateQuery = "UPDATE users SET first_name = ?, last_name = ?, phone = ?, address = ?  WHERE email = ?";
 
     // Prepare and bind parameters
     $stmt = $conn->prepare($updateQuery);
-    $stmt->bind_param("ssssssssss", $firstname, $lastname, $email, $phone, $hashed_password, $address, $postal, $dob, $gender, $emailToUpdate);
+    $stmt->bind_param("sssss", $firstname, $lastname, $phone, $address, $emailToUpdate);
 
     // Execute the update statement
     if ($stmt->execute()) {
